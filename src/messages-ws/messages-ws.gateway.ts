339d53688c573d2b3aafad9b4d3320a57c8268fd
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-11-14 20:42:57 -0500

    finished websocket

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -8,6 +8,8 @@ import {
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
 import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
@@ -15,11 +17,22 @@ export class MessagesWsGateway
 {
   @WebSocketServer() wss: Server;
 
-  constructor(private readonly messagesWsService: MessagesWsService) {}
-  handleConnection(client: Socket) {
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async handleConnection(client: Socket) {
     const token = client.handshake.headers.authentication as string;
-    console.log({ token });
-    this.messagesWsService.registerClient(client);
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      console.log(error);
+      client.disconnect();
+      return;
+    }
+    // console.log({ payload });
     this.wss.emit(
       'clients-updated',
       this.messagesWsService.getConnectedClients(),
@@ -48,7 +61,7 @@ export class MessagesWsGateway
     // });
     //! Emitir a todos menos
     this.wss.emit('messages-from-server', {
-      fullName: 'Soy yoo',
+      fullName: this.messagesWsService.getUserFullNameBySocketId(client.id),
       message: payload.message || 'No Message',
     });
   }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
+import { Repository } from 'typeorm';
+
+import { User } from '../auth/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 interface ConnectedClients {
-  [id: string]: Socket;
+  [id: string]: {
+    socket: Socket;
+    user: User;
+  };
 }
 
 @Injectable()
 export class MessagesWsService {
   private connectedClients: ConnectedClients = {};
 
-  registerClient(client: Socket) {
-    this.connectedClients[client.id] = client;
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async registerClient(client: Socket, userId: string) {
+    const user = await this.userRepository.findOneBy({ id: userId });
+    if (!user) throw new Error('User not found');
+    if (!user.isActive) throw new Error('User not active');
+    this.checkUserConecction(user);
+    this.connectedClients[client.id] = {
+      socket: client,
+      user,
+    };
   }
 
   removeClient(clientId: string) {
@@ -19,5 +37,20 @@ export class MessagesWsService {
 
   getConnectedClients(): string[] {
     return Object.keys(this.connectedClients);
+  }
+
+  getUserFullNameBySocketId(socketId: string) {
+    return this.connectedClients[socketId].user.fullName;
+  }
+
+  private checkUserConecction(user: User) {
+    for (const clientId of Object.keys(this.connectedClients)) {
+      const connectedClients = this.connectedClients[clientId];
+
+      if (connectedClients.user.id === user.id) {
+        connectedClients.socket.disconnect();
+        break;
+      }
+    }
   }
 }
